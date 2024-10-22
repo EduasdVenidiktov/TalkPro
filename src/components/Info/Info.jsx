73ce38e6bfe63a275f017ea114afffb85160aa7f
Eduard Venidiktov <edven7777777@gmail.com>
2024-10-22 22:14:36 +0300

    complete homePage`s style

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,15 +1,19 @@
-import css from './Info.module.css'
+import css from './Info.module.css' // Імпортуєш модульні стилі для інших елементів
 
 export function Info() {
   return (
+    // Тут не використовуй `css.wrapper`, а просто глобальний клас `wrapper`
     <div className={css.infoSection}>
-      <h1>Unlock your potential with the best language tutors</h1>
-      <p>
+      <h1 className={css.title}>
+        Unlock your potential with the best
+        <span className={css.spanWord}> language </span> tutors
+      </h1>
+      <p className={css.infoText}>
         Embark on an Exciting Language Journey with Expert Language Tutors:
         Elevate your language proficiency to new heights by connecting with
         highly qualified and experienced tutors.
       </p>
-      <button>Get started</button>
+      <button className={css.buttonStyle}>Get started</button>
     </div>
   )
 }
