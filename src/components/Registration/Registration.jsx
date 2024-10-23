5fa7d3831e203559ee90c17191a029a1ccc30e53
Eduard Venidiktov <edven7777777@gmail.com>
2024-10-23 16:34:00 +0300

    add css logIn registration

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,30 +1,28 @@
 import css from './Registration.module.css'
+import close from '../../assets/icons/sprite.svg'
 
 export function Registration() {
   return (
     <div className={css.registrationSection}>
-      <button type="button">
-        <svg className={css.close} aria-label="close log in Icon">
-          <use href={`${close}#close`} />
-        </svg>
-      </button>
+      <svg className={css.closeIcon} aria-label="close log in Icon">
+        <use href={`${close}#close`} />
+      </svg>
 
       <h2 className={css.titleRegistration}>Registration</h2>
       <p className={css.textRegistration}>
         Thank you for your interest in our platform! In order to register, we
         need some information. Please provide us with the following information
       </p>
-      <ul className={css.listRegistration}>
-        <li className={css.itemRegistration}>
-          <input type="text" placeholder="Name" />
-        </li>
-        <li className={css.itemRegistration}>
-          <input type="mail" placeholder="Email" />
-        </li>
-        <li className={css.itemRegistration}>
-          <input type="Password" placeholder="Password" />
-        </li>
-      </ul>
+
+      <div className={css.inputWrapper}>
+        <input type="text" placeholder="Name" className={css.inputField} />
+        <input type="mail" placeholder="Email" className={css.inputField} />
+        <input
+          type="Password"
+          placeholder="Password"
+          className={css.inputField}
+        />
+      </div>
       <button type="submit" className={css.btnSignIn}>
         Sign In
       </button>
