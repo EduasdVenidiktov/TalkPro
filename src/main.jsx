9e2c4e7783726cd3bcc164de55899b4c2e94ec02
Eduard Venidiktov <edven7777777@gmail.com>
2024-10-23 22:30:18 +0300

    install firebase

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPA9oF26HyhwskD8CNDauOE7kzE1lzfbQ',
+  authDomain: 'talkpro-ce5a4.firebaseapp.com',
+  databaseURL: 'https://talkpro-ce5a4-default-rtdb.firebaseio.com',
+  projectId: 'talkpro-ce5a4',
+  storageBucket: 'talkpro-ce5a4.appspot.com',
+  messagingSenderId: '255413950810',
+  appId: '1:255413950810:web:0b152b3d7c10f36dae3b63',
+}
+const app = initializeApp(firebaseConfig)
+getDatabase(app)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
