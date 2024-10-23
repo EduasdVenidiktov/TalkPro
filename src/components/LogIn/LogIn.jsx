fbe7eb60374cac042c4c9db1e5d7fdac69ebbc84
Eduard Venidiktov <edven7777777@gmail.com>
2024-10-23 16:34:00 +0300

    add css logIn registration

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -4,18 +4,22 @@ import close from '../../assets/icons/sprite.svg'
 export function LogIn() {
   return (
     <div className={css.LoginSection}>
-      <button type="button">
-        <svg className={css.close} aria-label="close log in Icon">
-          <use href={`${close}#close`} />
-        </svg>
-      </button>
+      <svg className={css.closeIcon} aria-label="close log in Icon">
+        <use href={`${close}#x`} />
+      </svg>
       <h2 className={css.titleLogin}>Log In</h2>
       <p className={css.textLogin}>
         Welcome back! Please enter your credentials to access your account and
         continue your search for an teacher.
       </p>
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <div className={css.inputWrapper}>
+        <input type="text" placeholder="Email" className={css.inputField} />
+        <input
+          type="password"
+          placeholder="Password"
+          className={css.inputField}
+        />
+      </div>
       <button type="submit" className={css.btnLogin}>
         Log In
       </button>
