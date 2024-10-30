66710ff0d40c223f7f55f5d5cc4d67c5b63f6f7c
Eduard Venidiktov <edven7777777@gmail.com>
2024-10-31 00:02:05 +0200

    add logic&style TeachersPage

--- src/pages/TeachersPage/TeacherCard/TeacherCard.jsx
+++ src/pages/TeachersPage/TeacherCard/TeacherCard.jsx
@@ -1,131 +1,12 @@
-// import { useState } from 'react'
-// import css from './TeacherCard.module.css'
-// import bookOpen from '/src/assets/icons/sprite.svg'
-// import star from '/src/assets/icons/sprite.svg'
-// import { FaHeart } from 'react-icons/fa'
-// import { FiHeart } from 'react-icons/fi'
-// import { useFavorites } from '../../FavoritesPage/FavoritesPage'
-
-// export function TeacherCard({
-//   _id, // Добавляем _id как пропс для идентификации учителя
-//   name,
-//   surname,
-//   languages = [],
-//   levels = [],
-//   rating,
-//   price_per_hour,
-//   avatar_url,
-//   lessons_done,
-//   lesson_info,
-//   conditions,
-//   experience,
-//   reviews = [],
-// }) {
-//   const [showMore, setShowMore] = useState(false)
-//   const { addFavorite, removeFavorite, isFavorite } = useFavorites()
-
-//   const handleReadMore = () => {
-//     setShowMore((prev) => !prev)
-//   }
-
-//   const handleHeartClick = () => {
-//     if (isFavorite(_id)) {
-//       // Проверяем, есть ли учитель в избранном
-//       removeFavorite(_id)
-//     } else {
-//       addFavorite({
-//         _id,
-//         name,
-//         surname,
-//         languages,
-//         levels,
-//         rating,
-//         price_per_hour,
-//         avatar_url,
-//         lessons_done,
-//         lesson_info,
-//         conditions,
-//         experience,
-//         reviews,
-//       }) // Добавляем учителя в избранное
-//     }
-//   }
-
-//   return (
-//     <div className={css.teacherSection}>
-//       <div className={css.teacherCard}>
-//         <div className={css.teacherHeader}>
-//           <img
-//             src={avatar_url}
-//             alt={`${name} ${surname}`}
-//             className={css.teacherImage}
-//           />
-//           <p className={css.speaksText}>Languages</p>
-//           <svg className={css.bookOpenIcon} aria-label="open book">
-//             <use href={`${bookOpen}#bookOpen`} />
-//           </svg>
-//           <p>Lessons online</p>
-//           <p className={css.lessonsDone}>Lessons done: {lessons_done}</p>
-//           <svg className={css.bookOpenIcon} aria-label="open book">
-//             <use href={`${star}#star`} />
-//           </svg>
-//           <p className={css.ratingText}>Rating: {rating}</p>
-//           <p>
-//             Price/ 1 hour:{' '}
-//             <span className={css.priceValue}>{price_per_hour}$</span>
-//           </p>
-
-//           <button className={css.btnHeart} onClick={handleHeartClick}>
-//             {isFavorite(_id) ? (
-//               <FaHeart className={css.iconSize} />
-//             ) : (
-//               <FiHeart className={css.iconSize} />
-//             )}
-//           </button>
-//         </div>
-
-//         <div className={css.teacherInfo}>
-//           <h2 className={css.titleTeachers}>
-//             {name} {surname}
-//           </h2>
-//           <p className={css.speaksText}>Speaks: {languages.join(', ')}</p>
-//           <p className={css.lessonInfo}>Lesson info: {lesson_info}</p>
-//           <p className={css.conditions}>Conditions: {conditions}</p>
-
-//           <button onClick={handleReadMore} className={css.btnShowSearch}>
-//             {showMore ? 'Show less' : 'Read more'}
-//           </button>
-//           {showMore && (
-//             <div className={css.additionalInfo}>
-//               <p className={css.teacherExperience}>{experience}</p>
-//               {reviews.map((review, index) => (
-//                 <div key={index} className={css.review}>
-//                   <p className={css.reviewerName}>{review.reviewer_name}</p>
-//                   <div className={css.teacherRating}>
-//                     <svg className={css.bookOpenIcon} aria-label="open book">
-//                       <use href={`${star}#star`} />
-//                     </svg>
-//                     <p className={css.ratingValue}>{review.reviewer_rating}</p>
-//                   </div>
-//                   <p className={css.reviewComment}>{review.comment}</p>
-//                 </div>
-//               ))}
-//             </div>
-//           )}
-//         </div>
-
-//         <div className={css.levelList}>
-//           <p className={css.levelsText}>Levels: {levels.join(', ')}</p>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
 import { useState } from 'react'
 import css from './TeacherCard.module.css'
 import bookOpen from '/src/assets/icons/sprite.svg'
 import star from '/src/assets/icons/sprite.svg'
+import greenLable from '/src/assets/icons/sprite.svg'
+
+import { FaHeart } from 'react-icons/fa'
+import { FiHeart } from 'react-icons/fi'
+import { Lesson } from '/src/components/Modal/Lesson/Lesson'
 
 export function TeacherCard({
   name,
@@ -139,73 +20,273 @@ export function TeacherCard({
   lesson_info,
   conditions,
   experience,
-  reviews = [], // Добавляем пропс для отзывов
+  reviews = [],
 }) {
   const [showMore, setShowMore] = useState(false)
+  const [isFavorite, setIsFavorite] = useState(false) // Додано для збереження статусу улюбленого
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  // Функция для открытия и закрытия модального окна
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
 
   const handleReadMore = () => {
     setShowMore((prev) => !prev)
   }
 
+  const handleHeartClick = () => {
+    setIsFavorite((prev) => !prev) // Змінюємо статус улюбленого при натисканні
+  }
+
   return (
     <div className={css.teacherSection}>
       <div className={css.teacherCard}>
-        <div className={css.teacherHeader}>
+        <div className={css.teacherImageBox}>
           <img
             src={avatar_url}
             alt={`${name} ${surname}`}
             className={css.teacherImage}
           />
-          <p className={css.speaksText}>Languages</p>
-          <svg className={css.bookOpenIcon} aria-label="open book">
-            <use href={`${bookOpen}#bookOpen`} />
+          <svg className={css.teacherLable} aria-label="green lable">
+            <use href={`${greenLable}#greenLable`} />
           </svg>
-          <p>Lessons online</p>
-          <p className={css.lessonsDone}>Lessons done: {lessons_done}</p>
-          <svg className={css.bookOpenIcon} aria-label="open book">
-            <use href={`${star}#star`} />
-          </svg>
-          <p className={css.ratingText}>Rating: {rating}</p>
-          <p>
-            Price/ 1 hour:{' '}
-            <span className={css.priceValue}>{price_per_hour}$</span>
-          </p>
         </div>
+        <div className={css.teacherContent}>
+          <div className={css.teacherHeader}>
+            <p className={css.contentItem}>Languages</p>
 
-        <div className={css.teacherInfo}>
-          <h2 className={css.titleTeachers}>
-            {name} {surname}
-          </h2>
-          <p className={css.speaksText}>Speaks: {languages.join(', ')}</p>
-          <p className={css.lessonInfo}>Lesson info: {lesson_info}</p>
-          <p className={css.conditions}>Conditions: {conditions}</p>
+            <div className={css.teacherHeaderList}>
+              <svg className={css.teacherIcon} aria-label="open book">
+                <use href={`${bookOpen}#bookOpen`} />
+              </svg>
+              <p className={css.teacherContent}>Lessons online</p>
+              <span className={css.divider}></span>
 
-          <button onClick={handleReadMore} className={css.btnShowSearch}>
-            {showMore ? 'Show less' : 'Read more'}
-          </button>
-          {showMore && (
-            <div className={css.additionalInfo}>
-              <p className={css.teacherExperience}>{experience}</p>
-              {reviews.map((review, index) => (
-                <div key={index} className={css.review}>
-                  <p className={css.reviewerName}>{review.reviewer_name}</p>
-                  <div className={css.teacherRating}>
-                    <svg className={css.bookOpenIcon} aria-label="open book">
-                      <use href={`${star}#star`} />
-                    </svg>
-                    <p className={css.ratingValue}>{review.reviewer_rating}</p>
-                  </div>
-                  <p className={css.reviewComment}>{review.comment}</p>
-                </div>
-              ))}
+              <p className={css.teacherContent}>Lessons done: {lessons_done}</p>
+              <span className={css.divider}></span>
+
+              <svg className={css.teacherIcon} aria-label="open book">
+                <use href={`${star}#star`} />
+              </svg>
+              <p className={css.teacherContent}>Rating: {rating}</p>
+              <span className={css.divider}></span>
+
+              <p className={css.teacherContent}>
+                Price/ 1 hour:{' '}
+                <span className={css.priceValue}>{price_per_hour}$</span>
+              </p>
             </div>
-          )}
-        </div>
 
-        <div className={css.levelList}>
-          <p className={css.levelsText}>Levels: {levels.join(', ')}</p>
+            <p className={css.btnHeart} onClick={handleHeartClick}>
+              {isFavorite ? (
+                <FaHeart className={`${css.heartIcon} ${css.favorited}`} />
+              ) : (
+                <FiHeart className={css.heartIcon} />
+              )}
+            </p>
+          </div>
+
+          <div className={css.teacherInfo}>
+            <h2 className={css.titleTeachers}>
+              {name} {surname}
+            </h2>
+            <ul className={css.contentList}>
+              <li className={css.contentItem}>
+                Speaks:{' '}
+                <span className={css.teacherContent}>
+                  {languages.join(', ')}
+                </span>
+              </li>
+              <li className={css.contentItem}>
+                Lesson info:{' '}
+                <span className={css.teacherContent}>{lesson_info}</span>
+              </li>
+              <li className={css.contentItem}>
+                Conditions:{' '}
+                <span className={css.teacherContent}>{conditions}</span>
+              </li>
+            </ul>
+
+            <p onClick={handleReadMore} className={css.linkShowSearch}>
+              {showMore ? 'Show less' : 'Read more'}
+            </p>
+            {showMore && (
+              <div className={css.additionalInfo}>
+                <p className={css.experienceContent}>{experience}</p>
+                {reviews.map((review, index) => (
+                  <div key={index} className={css.review}>
+                    <p className={css.contentItem}>{review.reviewer_name}</p>
+                    <div className={css.teacherRating}>
+                      <svg className={css.teacherIcon} aria-label="open book">
+                        <use href={`${star}#star`} />
+                      </svg>
+                      <p className={css.ratingValue}>
+                        {review.reviewer_rating}
+                      </p>
+                    </div>
+                    <p className={css.reviewComment}>{review.comment}</p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+
+          <div className={css.levelList}>
+            {levels.map((level, index) => (
+              <p key={index} className={css.levelsItem}>
+                {level}
+              </p>
+            ))}
+          </div>
+          {showMore && (
+            <button
+              onClick={openModal}
+              type="button"
+              className={css.btnTrialLesson}
+            >
+              Book trial lesson
+            </button>
+          )}
+
+          {/* Отображаем модальное окно Lesson, если isModalOpen === true */}
+          {isModalOpen && <Lesson onClose={closeModal} />}
         </div>
       </div>
     </div>
   )
 }
+
+// import { useState } from 'react'
+// import css from './TeacherCard.module.css'
+// import bookOpen from '/src/assets/icons/sprite.svg'
+// import star from '/src/assets/icons/sprite.svg'
+// import greenLable from '/src/assets/icons/sprite.svg'
+
+// import { FaHeart } from 'react-icons/fa'
+// import { FiHeart } from 'react-icons/fi'
+
+// export function TeacherCard({
+//   name,
+//   surname,
+//   languages = [],
+//   levels = [],
+//   rating,
+//   price_per_hour,
+//   avatar_url,
+//   lessons_done,
+//   lesson_info,
+//   conditions,
+//   experience,
+//   reviews = [], // Добавляем пропс для отзывов
+// }) {
+//   const [showMore, setShowMore] = useState(false)
+
+//   const handleReadMore = () => {
+//     setShowMore((prev) => !prev)
+//   }
+
+//   return (
+//     <div className={css.teacherSection}>
+//       <div className={css.teacherCard}>
+//         <div className={css.teacherImageBox}>
+//           <img
+//             src={avatar_url}
+//             alt={`${name} ${surname}`}
+//             className={css.teacherImage}
+//           />
+//           <svg className={css.teacherLable} aria-label="green lable">
+//             <use href={`${greenLable}#greenLable`} />
+//           </svg>
+//         </div>
+//         <div className={css.teacherContent}>
+//           <div className={css.teacherHeader}>
+//             <p className={css.contentItem}>Languages</p>
+
+//             <div className={css.teacherHeaderList}>
+//               <svg className={css.teacherIcon} aria-label="open book">
+//                 <use href={`${bookOpen}#bookOpen`} />
+//               </svg>
+//               <p className={css.teacherContent}>Lessons online</p>
+//               <span className={css.divider}></span>
+
+//               <p className={css.teacherContent}>Lessons done: {lessons_done}</p>
+//               <span className={css.divider}></span>
+
+//               <svg className={css.teacherIcon} aria-label="open book">
+//                 <use href={`${star}#star`} />
+//               </svg>
+//               <p className={css.teacherContent}>Rating: {rating}</p>
+//               <span className={css.divider}></span>
+
+//               <p className={css.teacherContent}>
+//                 Price/ 1 hour:{' '}
+//                 <span className={css.priceValue}>{price_per_hour}$</span>
+//               </p>
+//             </div>
+//             <FiHeart className={css.heartIcon} />
+//             {/* <button className={css.btnHeart} onClick={handleHeartClick}>
+//               {isFavorite(camper._id) ? (
+//                 <FaHeart className={css.iconSize} />
+//               ) : (
+//                 <FiHeart className={css.iconSize} />
+//               )}
+//             </button> */}
+//           </div>
+
+//           <div className={css.teacherInfo}>
+//             <h2 className={css.titleTeachers}>
+//               {name} {surname}
+//             </h2>
+//             <ul className={css.contentList}>
+//               <li className={css.contentItem}>
+//                 Speaks:{' '}
+//                 <span className={css.teacherContent}>
+//                   {languages.join(', ')}
+//                 </span>
+//               </li>
+//               <li className={css.contentItem}>
+//                 Lesson info:{' '}
+//                 <span className={css.teacherContent}>{lesson_info}</span>
+//               </li>
+//               <li className={css.contentItem}>
+//                 Conditions:{' '}
+//                 <span className={css.teacherContent}>{conditions}</span>
+//               </li>
+//             </ul>
+
+//             <p onClick={handleReadMore} className={css.linkShowSearch}>
+//               {showMore ? 'Show less' : 'Read more'}
+//             </p>
+//             {showMore && (
+//               <div className={css.additionalInfo}>
+//                 <p className={css.experienceContent}>{experience}</p>
+//                 {reviews.map((review, index) => (
+//                   <div key={index} className={css.review}>
+//                     <p className={css.contentItem}>{review.reviewer_name}</p>
+//                     <div className={css.teacherRating}>
+//                       <svg className={css.teacherIcon} aria-label="open book">
+//                         <use href={`${star}#star`} />
+//                       </svg>
+//                       <p className={css.ratingValue}>
+//                         {review.reviewer_rating}
+//                       </p>
+//                     </div>
+//                     <p className={css.reviewComment}>{review.comment}</p>
+//                   </div>
+//                 ))}
+//               </div>
+//             )}
+//           </div>
+
+//           <div className={css.levelList}>
+//             {levels.map((level, index) => (
+//               <p key={index} className={css.levelsItem}>
+//                 {level}
+//               </p>
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
--- src/pages/TeachersPage/TeachersPage.jsx
+++ src/pages/TeachersPage/TeachersPage.jsx
@@ -1,15 +1,8 @@
-// import css from './Teachers.module.css'
-// import { ref, get } from 'firebase/database'
-// import { useState, useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { setTeachers } from './redux/teachersSlice' // Імпорт дії для збереження в Redux (якщо використовується)
-// import TeacherCard from '../../components/TeacherCard/TeacherCard'
-
-// TeachersPage.jsx
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { TeacherCard } from './TeacherCard/TeacherCard'
 import { database, ref, get, child } from '../../firebase/firebase' // Подключите Firebase
+import { Filters } from '/src/components/Filters/Filters'
 
 export function TeachersPage() {
   const [teachers, setTeachers] = useState([])
@@ -40,50 +33,10 @@ export function TeachersPage() {
   return (
     <div>
       <Link to="/">Go to main page</Link>
-      <h1>TeacherPage</h1>
+      <Filters />
       {teachers.map((teacher) => (
         <TeacherCard key={teacher.id} {...teacher} />
       ))}
     </div>
   )
 }
-
-// // Функція для отримання даних про викладачів
-// export function TeachersPage(database) {
-//   const dbRef = ref(database, 'teachers')
-//   return get(dbRef)
-//     .then((snapshot) => {
-//       if (snapshot.exists()) {
-//         return snapshot.val()
-//       } else {
-//         console.log('Дані не знайдено')
-//         return []
-//       }
-//     })
-//     .catch((error) => {
-//       console.error('Помилка при отриманні даних:', error)
-//       return []
-//     })
-// }
-
-// // Використання функції у компоненті
-// function TeachersPage() {
-//   const [teachers, setTeachers] = useState([])
-//   const dispatch = useDispatch()
-//   const database = getDatabase() // Використовуйте вашу ініціалізовану базу даних Firebase
-
-//   useEffect(() => {
-//     fetchTeachers(database).then((data) => {
-//       setTeachers(data)
-//       dispatch(setTeachers(data)) // Додавання до Redux, якщо потрібно
-//     })
-//   }, [database, dispatch])
-
-//   return (
-//     <div>
-//       {teachers.map((teacher, index) => (
-//         <TeacherCard key={index} {...teacher} />
-//       ))}
-//     </div>
-//   )
-// }
