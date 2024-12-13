7ee4a2ed0711d32de8a47aa92649b63ad8b427e7
Eduard Venidiktov <edven7777777@gmail.com>
2024-12-13 21:34:45 +0200

    add header logic change buttons logOut to LogIn&Registration

--- src/pages/HomePage/HomeHeader/HomeHeader.jsx
+++ src/pages/HomePage/HomeHeader/HomeHeader.jsx
@@ -4,7 +4,7 @@ import ukraine from '../../../assets/icons/sprite.svg'
 import logOut from '../../../assets/icons/sprite.svg'
 import { LogIn } from '../../../components/Modal/LogIn/LogIn' // Імпортуємо компонент LogIn
 import { Registration } from '../../../components/Modal/Registration/Registration' // Імпортуємо компонент Registration
-import { Link, useNavigate } from 'react-router-dom' // Імпорт Link для навігації
+import { Link } from 'react-router-dom' // Імпорт Link для навігації
 
 import toast from 'react-hot-toast'
 
@@ -13,13 +13,19 @@ export function HomeHeader() {
   const [isRegistrationOpen, setIsRegistrationOpen] = useState(false)
   const [isAuthenticated, setIsAuthenticated] = useState(false)
 
+  const [hasFavorites, setHasFavorites] = useState(false)
+
   useEffect(() => {
     // Перевіряємо, чи є токен в localStorage
     const userToken = localStorage.getItem('userToken')
     setIsAuthenticated(!!userToken)
+
+    const favoriteCards =
+      JSON.parse(localStorage.getItem('favoriteCards')) || []
+    setHasFavorites(favoriteCards.length > 0) // Оновлюємо стан
   })
 
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
   const handleLogInClick = () => {
     setIsLogInOpen(true)
@@ -40,11 +46,15 @@ export function HomeHeader() {
     localStorage.removeItem('favorites') // Очищення вибраних сердечок
     sessionStorage.clear() // Очищення тимчасових даних
     setIsAuthenticated(false) // Оновлюємо стан аутентифікації
+    setHasFavorites(false) // Оновлюємо стан hasFavorites, щоб хедер більше не відображав "Favorite"
+    setIsLogInOpen(false)
+    setIsRegistrationOpen(false)
+
     toast.success('Buy, see you again!', {
       className: css.toastSuccess,
       duration: 1500, // Продолжительность в миллисекундах (5000 = 5 секунд)
     })
-    navigate('/') // Перенаправлення на головну
+    // navigate('/') // Перенаправлення на головну
   }
 
   const handleLogInSuccess = (user) => {
@@ -77,7 +87,7 @@ export function HomeHeader() {
             Teachers
           </Link>
         </li>
-        {isAuthenticated && (
+        {isAuthenticated && hasFavorites && (
           <li>
             <Link to="/favorite" className={css.menuItem}>
               Favorite
@@ -130,119 +140,3 @@ export function HomeHeader() {
     </div>
   )
 }
-
-// import { useState } from 'react'
-// import css from './HomeHeader.module.css'
-// import ukraine from '../../../assets/icons/sprite.svg'
-// import logOut from '../../../assets/icons/sprite.svg'
-// import { LogIn } from '../../../components/Modal/LogIn/LogIn' // Імпортуємо компонент LogIn
-// import { Registration } from '../../../components/Modal/Registration/Registration' // Імпортуємо компонент Registration
-// // import { Link, useNavigate } from 'react-router-dom' // Імпорт Link для навігації
-// import { Link } from 'react-router-dom' // Імпорт Link для навігації
-
-// import toast from 'react-hot-toast'
-// export function HomeHeader() {
-//   const [isLogInOpen, setIsLogInOpen] = useState(false)
-//   const [isRegistrationOpen, setIsRegistrationOpen] = useState(false)
-
-//   // const navigate = useNavigate()
-
-//   const handleLogInClick = () => {
-//     setIsLogInOpen(true)
-//   }
-
-//   const handleRegistrationClick = () => {
-//     setIsRegistrationOpen(true)
-//   }
-
-//   const closeModal = () => {
-//     setIsLogInOpen(false)
-//     setIsRegistrationOpen(false)
-//   }
-
-//   const handleLogOut = () => {
-//     localStorage.removeItem('userToken') // Видаляємо токен
-//     localStorage.removeItem('filters') // Очищення вибраних фільтрів
-//     localStorage.removeItem('favorites') // Очищення вибраних сердечок
-//     sessionStorage.clear() // Очищення тимчасових даних
-//     toast.success('Buy, see you again!', {
-//       className: css.toastSuccess,
-//       duration: 1500, // Продолжительность в миллисекундах (5000 = 5 секунд)
-//     })
-//     // navigate('/') // Перенаправлення на головну
-//   }
-
-//   // const handleLogOut = () => {
-//   //   localStorage.removeItem('userToken') // Удаляем токен из localStorage
-//   //   sessionStorage.clear() // Повністю очищаємо sessionStorage, якщо воно використовується лише для тимчасових даних
-//   //   navigate('/') // Перенаправляем на главную
-//   //   alert('Користувач вийшов')
-//   // }
-
-//   return (
-//     <div className={css.headerBox}>
-//       <div className={css.logoHeaderBox}>
-//         <svg className={css.logoImg} aria-label="Logo Ukraine Icon">
-//           <use href={`${ukraine}#ukraine`} />
-//         </svg>
-//         <h2>
-//           <Link to="/" className={css.logoName}>
-//             LearnLingo
-//           </Link>
-//         </h2>
-//       </div>
-
-//       <ul className={css.headerMenu}>
-//         <li>
-//           <Link to="/" className={css.menuItem}>
-//             Home
-//           </Link>
-//         </li>
-//         <li>
-//           <Link to="/teachers" className={css.menuItem}>
-//             Teachers
-//           </Link>
-//         </li>
-//         <li>
-//           <Link to="/favorite" className={css.menuItem}>
-//             Favorite
-//           </Link>
-//         </li>
-//       </ul>
-
-//       <ul className={css.regAuthMenu}>
-//         <li>
-//           <svg
-//             className={css.logOut}
-//             aria-label="Log In Icon"
-//             onClick={handleLogOut}
-//           >
-//             <use href={`${logOut}#logOut`} />
-//           </svg>
-//         </li>
-//         <li>
-//           <h2 className={css.loginText} onClick={handleLogInClick}>
-//             Log in
-//           </h2>
-//         </li>
-//         <li>
-//           <button className={css.buttonReg} onClick={handleRegistrationClick}>
-//             Registration
-//           </button>
-//         </li>
-//       </ul>
-
-//       {/* Відкриваємо модальні вікна */}
-//       {isLogInOpen && (
-//         <div className={css.modalOverlay}>
-//           <LogIn onClose={closeModal} />
-//         </div>
-//       )}
-//       {isRegistrationOpen && (
-//         <div className={css.modalOverlay}>
-//           <Registration onClose={closeModal} />
-//         </div>
-//       )}
-//     </div>
-//   )
-// }
