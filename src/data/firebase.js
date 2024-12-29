f61614879f01c07766ae9d8788e4ffc5d6c58cc2
Eduard Venidiktov <edven7777777@gmail.com>
2024-12-30 00:11:37 +0200

    correct

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -12,10 +12,19 @@ import {
   getDocs,
 } from 'firebase/firestore'
 
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCPA9oF26HyhwskD8CNDauOE7kzE1lzfbQ',
-  authDomain: 'talkpro-ce5a4.firebaseapp.com',
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// }
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPA9oF26HyhwskD8CNDauOE7kzE1lzfbQ',
+  authDomain: 'talkpro - ce5a4.firebaseapp.com',
   databaseURL: 'https://talkpro-ce5a4-default-rtdb.firebaseio.com',
   projectId: 'talkpro-ce5a4',
   storageBucket: 'talkpro-ce5a4.appspot.com',
